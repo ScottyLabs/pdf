4cc79274af34c26340a7720c8d36ecc8f411cdbb
justinzhang <justinz@andrew.cmu.edu>
2022-10-25 12:49:31 -0400

    added logo to initial landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,6 +11,7 @@ function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <header className={clsx("hero hero--primary", styles.heroBanner)}>
+      <img src={require("@site/static/img/pdf-logo.png").default} className={clsx("hero hero--primary", styles.center)}/>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
