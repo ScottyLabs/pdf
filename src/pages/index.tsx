80e734742f961fcb4a3b125ee8c8e12529ba8b19
justinzhang <justinz@andrew.cmu.edu>
2022-10-25 22:07:02 -0400

    added media queries for margins, except title margins (unsure how to do this)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,6 @@ import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import HomepageFeatures from "@site/src/components/HomepageFeatures";
-
 import styles from "./index.module.css";
 
 function HomepageHeader() {
@@ -13,8 +12,8 @@ function HomepageHeader() {
     <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <img src={require("@site/static/img/pdf-logo.png").default} className={clsx("hero hero--primary", styles.center)}/>
       <div className="container">
-        <h1 className="hero__title" style={{paddingInline: "10rem"}}>{siteConfig.title}</h1>
-        <p className="hero__subtitle" style={{paddingInline: "10rem"}}>{siteConfig.tagline}</p>
+        <h1 className="hero__title" >{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
