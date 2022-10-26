2f8722791302af74af2451021466f1dd0c66e193
Gram Liu <gram.liu226@gmail.com>
2022-10-25 23:03:00 -0400

    Make schedule closer to top. Add ScottyLabs logo

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -4,7 +4,8 @@ import styles from "./styles.module.css";
 
 type FeatureItem = {
   title: string;
-  Svg: React.ComponentType<React.ComponentProps<"svg">>;
+  Svg?: React.ComponentType<React.ComponentProps<"svg">>;
+  imgUrl?: string;
   description: JSX.Element;
 };
 
@@ -14,35 +15,51 @@ const FeatureList: FeatureItem[] = [
     Svg: require("@site/static/img/undraw_docusaurus_react.svg").default,
     description: (
       <>
-        Whether you want to learn a widely-applicable skill, applying for full time jobs, or somewhere
-        in between, our workshops are designed to provide you the skills to create a aesthetic and functional
-        personal website!
-
-        Our workshop focuses on the fundementals of web development with activities that
-        will walk you through the development and deployment of your personal website! We
-        have a beginner track, more focused on HTML and CSS, and an advanced track, more focused
-        on Javascript integration. At the end of the day, you will have a deployed website and a
-        good understanding of the basics of web development.
+        Portfolio Development Fair is an annual series of student-led workshops
+        and talks that introduce you to web development centered around
+        developing your own personal website. It is a great place to pick up new
+        skills that you'll be able to use in your projects, hackathons, and
+        classes!
       </>
     ),
   },
   {
     title: "Who we are",
-    Svg: require("@site/static/img/undraw_docusaurus_mountain.svg").default,
+    imgUrl: require("@site/static/img/scottylabs.png").default,
     description: (
       <>
-        Portfolio Development Fair is run by <a href="https://www.scottylabs.org/">ScottyLabs</a>. We also run <a href="https://tartanhacks.com/">TartanHacks</a> (CMU’s largest software hackathon) and other educational events like <a href="https://crashcourse.scottylabs.org/">CrashCourse</a>.
+        Portfolio Development Fair is run by{" "}
+        <a href="https://www.scottylabs.org/" target="_blank" rel="noreferrer">
+          ScottyLabs
+        </a>
+        . We also run{" "}
+        <a href="https://tartanhacks.com/" target="_blank" rel="noreferrer">
+          TartanHacks
+        </a>{" "}
+        (CMU’s largest software hackathon) and other educational events like{" "}
+        <a
+          href="https://crashcourse.scottylabs.org/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          CrashCourse
+        </a>
+        .
       </>
     ),
   },
 ];
 
-function Feature({ title, Svg, description }: FeatureItem) {
+function Feature({ title, Svg, imgUrl, description }: FeatureItem) {
+  const icon =
+    Svg == null ? (
+      <img src={imgUrl} className={styles.featureImage} />
+    ) : (
+      <Svg className={styles.featureSvg} role="img" />
+    );
   return (
     <div className={clsx("text--center --10")}>
-      <div className="">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
+      <div>{icon}</div>
       <div className={styles.featureItem}>
         <h3>{title}</h3>
         <p>{description}</p>
@@ -51,30 +68,26 @@ function Feature({ title, Svg, description }: FeatureItem) {
   );
 }
 
-export default function HomepageFeatures(): JSX.Element {
+function Schedule() {
   return (
-    <section className={styles.features}>
-      <div className="container">
-        <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
-        </div>
-
-      <div className="row" style={{display: "flex", flexDirection: "column", alignItems: "center", whiteSpace: "nowrap"}}>
-      <div className="text--center padding-horiz--md ">
-        <h3>Schedule</h3>
-        <p><table className={styles.table}>
+    <div className="text--center padding-horiz--md">
+      <h3>Schedule</h3>
+      <p>
+        <table className={styles.table}>
           <tr>
             <th>Session</th>
             <th>Time</th>
           </tr>
           <tr>
-            <td><a href="/Content">Content</a></td>
+            <td>
+              <a href="/content">Content</a>
+            </td>
             <td>11am - 12pm</td>
           </tr>
           <tr>
-            <td><a href="/Design">Design</a></td>
+            <td>
+              <a href="/design">Design</a>
+            </td>
             <td>12pm - 1pm</td>
           </tr>
           <tr>
@@ -82,25 +95,48 @@ export default function HomepageFeatures(): JSX.Element {
             <td>1pm - 2pm</td>
           </tr>
           <tr>
-            <td><a href="/Html">HTML</a> | <a href="/Javascript">Javascript</a></td>
+            <td>
+              <a href="/html">HTML</a> | <a href="/javascript">Javascript</a>
+            </td>
             <td>2pm - 3pm</td>
           </tr>
           <tr>
-            <td><a href="/Css">CSS</a> | <a href="/Javascript">Javascript</a></td>
+            <td>
+              <a href="/css">CSS</a> | <a href="/javascript">Javascript</a>
+            </td>
             <td>3pm - 4pm</td>
           </tr>
           <tr>
-            <td><a href="Deployment">Deployment</a></td>
+            <td>
+              <a href="deployment">Deployment</a>
+            </td>
             <td>4pm - 5pm</td>
           </tr>
           <tr>
             <td>Closing + Raffle</td>
             <td>5pm - 5:30pm</td>
           </tr>
-        </table></p>
-      </div>
-      </div>
+        </table>
+      </p>
+    </div>
+  );
+}
 
+export default function HomepageFeatures(): JSX.Element {
+  const [aboutFeature, clubFeature] = FeatureList;
+
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          <Feature {...aboutFeature} />
+        </div>
+        <div className={clsx("row", styles.centeredRow)}>
+          <Schedule />
+        </div>
+        <div className="row">
+          <Feature {...clubFeature} />
+        </div>
       </div>
     </section>
   );
